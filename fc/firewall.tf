6c5ab07dcf82edcc8c9981267b852eb9fdf9aace
Davide Principi <davide.principi@nethesis.it>
2021-02-17 18:01:35 +0100

    Add SSH only default firewall for fc

---
+++ fc/firewall.tf
@@ -0,0 +1,33 @@
+resource "digitalocean_firewall" "dp-ssh" {
+  name = "dp-ssh"
+
+  droplet_ids = [digitalocean_droplet.vps.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
